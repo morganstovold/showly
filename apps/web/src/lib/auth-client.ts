46370b0e239376ac387b2c26d139e75bb14b954d
Morgan Stovold <morgannstovold.dev@gmail.com>
2025-11-24 20:21:02 -0800

    initial commit

---
+++ apps/web/src/lib/auth-client.ts
@@ -0,0 +1,7 @@
+import type { auth } from "@showly/auth";
+import { createAuthClient } from "better-auth/react";
+import { inferAdditionalFields } from "better-auth/client/plugins";
+
+export const authClient = createAuthClient({
+	plugins: [inferAdditionalFields<typeof auth>()],
+});
